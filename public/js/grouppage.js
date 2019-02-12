82cb65fd5a88cbf51766a700f6e4712aa068e516
shinyscyther <mesibovj@gmail.com>
2019-02-12 16:23:30 -0500

    got user preferences stored as variables following user input in the relevant form

--- public/js/grouppage.js
+++ public/js/grouppage.js
@@ -1,1 +1,62 @@
-console.log("grouppage.js linked!");
+// console.log("grouppage.js linked!");
+
+$(function() {
+  $("#submit").on("click", function(event) {
+
+    let memberPref1 = $("#memberPref1").val();
+    console.log(memberPref1);
+
+    let memberPref2 = $("#memberPref2").val();
+    console.log(memberPref2);
+
+    let memberPref3 = $("#memberPref3").val();
+    console.log(memberPref3);
+
+    let memberPref4 = $("#memberPref4").val();
+    console.log(memberPref4);
+
+    let memberPref5 = $("#memberPref5").val();
+    console.log(memberPref5);
+
+    // below this is mostly code from one of the bootcamp activities using handlebars to use as reference.
+//     var newSleepState = {
+//       sleepy: newSleep
+//     };
+
+//     // Send the PUT request.
+//     $.ajax("/api/cats/" + id, {
+//       type: "PUT",
+//       data: newSleepState
+//     }).then(
+//       function() {
+//         console.log("changed sleep to", newSleep);
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".create-form").on("submit", function(event) {
+//     // Make sure to preventDefault on a submit event.
+//     event.preventDefault();
+
+//     var newCat = {
+//       name: $("#ca").val().trim(),
+//       sleepy: $("[name=sleepy]:checked").val().trim()
+//     };
+
+//     // Send the POST request.
+//     $.ajax("/api/cats", {
+//       type: "POST",
+//       data: newCat
+//     }).then(
+//       function() {
+//         console.log("created new cat");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+  });
+});
+
+// console.log("hello jordan");
