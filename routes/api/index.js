4ab0c9bba3b510efd3c7630168f99385faaa032e
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-12 12:17:47 -0500

    Updated api/index to take in new file groups.js.  Created groups.js with starting list of endpoints.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,9 @@ var models  = require('../../models');
 var express = require('express');
 var router  = express.Router();
 var users = require('./users.js');
+var groups = require('./groups.js');
 
 router.use('/users', users);
+router.use('/groups', groups);
 
 module.exports = router;
