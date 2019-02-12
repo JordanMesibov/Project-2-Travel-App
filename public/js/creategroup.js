d49d188c0a22f0084767a4f75a751d929cd879bd
shinyscyther <mesibovj@gmail.com>
2019-02-12 15:32:57 -0500

    working on capturing values of input fields for creategroup page

--- public/js/creategroup.js
+++ public/js/creategroup.js
@@ -1,8 +1,22 @@
-// $(function() {
-//   $("#submit").on("click", function(event) {
-//     let id = $(this).data("id");
-//     var newSleep = $(this).data("newsleep");
+$(function() {
+  $("#submit").on("click", function(event) {
 
+    let city1Name = $("#city1").val().trim();
+    console.log(city1Name);
+
+    let city2Name = $("#city2").val().trim();
+    console.log(city2Name);
+
+    let city3Name = $("#city3").val().trim();
+    console.log(city3Name);
+
+    let city4Name = $("#city4").val().trim();
+    console.log(city4Name);
+
+    let city5Name = $("#city5").val().trim();
+    console.log(city5Name);
+
+    // below this is mostly code from one of the bootcamp activities using handlebars to use as reference.
 //     var newSleepState = {
 //       sleepy: newSleep
 //     };
@@ -40,7 +54,7 @@
 //         location.reload();
 //       }
 //     );
-//   });
-// });
+  });
+});
 
 console.log("hello jordan");
