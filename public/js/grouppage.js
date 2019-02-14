e23532b70c2669c478aa1cc50d650bc6f7577b0e
shinyscyther <mesibovj@gmail.com>
2019-02-14 13:13:49 -0500

    removed commented out/garbage code from grouppage.js in the js folder in public

--- public/js/grouppage.js
+++ public/js/grouppage.js
@@ -18,44 +18,6 @@ $(function() {
     let memberPref5 = $("#memberPref5").val();
     console.log(memberPref5);
 
-    // below this is mostly code from one of the bootcamp activities using handlebars to use as reference.
-//     var newSleepState = {
-//       sleepy: newSleep
-//     };
-
-//     // Send the PUT request.
-//     $.ajax("/api/cats/" + id, {
-//       type: "PUT",
-//       data: newSleepState
-//     }).then(
-//       function() {
-//         console.log("changed sleep to", newSleep);
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
-//   });
-
-//   $(".create-form").on("submit", function(event) {
-//     // Make sure to preventDefault on a submit event.
-//     event.preventDefault();
-
-//     var newCat = {
-//       name: $("#ca").val().trim(),
-//       sleepy: $("[name=sleepy]:checked").val().trim()
-//     };
-
-//     // Send the POST request.
-//     $.ajax("/api/cats", {
-//       type: "POST",
-//       data: newCat
-//     }).then(
-//       function() {
-//         console.log("created new cat");
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
   });
 });
 
