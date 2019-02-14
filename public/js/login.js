54d4cb5973ad1ed159d2bb751865542acb6a608e
unknown <pp_nancy@hotmail.com>
2019-02-14 15:13:12 -0500

    Fixed null data

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
-
+  
 $( "#logInBtn" ).click(function(e) {
   e.preventDefault();
     console.log("this hit");
+
+    $("form input").each(function(i) {
+      if (!$(this).val()) {
+        $(this).addClass("is-invalid");
+        return false;
+      } else {
+        $(this).removeClass("is-invalid").addClass("is-valid");
+      }
+    })
+
     const userInfo = {
-    
       email: $("#email-input").val().trim(),
       password: $("#password-input").val().trim()
     };
+
+   
+  
 
     $.ajax({
       url: '/api/users/login',
@@ -15,9 +27,11 @@ $( "#logInBtn" ).click(function(e) {
       data: userInfo
     })
     .then((userInfo) => {
-      location.replace(userInfo)
+      // location.replace(userInfo);
+      location.replace("/dashboard");
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log("Your password or username incorrect!"+err));
+     
   });
 
 
