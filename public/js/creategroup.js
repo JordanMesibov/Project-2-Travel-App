e2cba2c2d5b6ae17c70ba1fa9511b049c70dee58
shinyscyther <mesibovj@gmail.com>
2019-02-14 16:10:12 -0500

    added event.preventDefault(); line to creategroup.js and commented out the .then function that triggered a location.reload(); for now so that I can make sure the data I am working with gets logged to the console

--- public/js/creategroup.js
+++ public/js/creategroup.js
@@ -1,7 +1,9 @@
 $(function () {
   $("#submit").on("click", function (event) {
+    event.preventDefault();
 
     let groupName = $("#groupName").val().trim();
+    console.log("The groupName is:");
     console.log(groupName);
 
     let city1Name = $("#city1").val().trim();
@@ -9,6 +11,11 @@ $(function () {
     let city3Name = $("#city3").val().trim();
     let city4Name = $("#city4").val().trim();
     let city5Name = $("#city5").val().trim();
+    console.log("The cities that have been entered are:");
+    console.log(city1Name);
+    console.log(city2Name);
+    console.log(city3Name);
+    console.log(city4Name);
     console.log(city5Name);
 
     let vacaInfo = {
@@ -26,7 +33,10 @@ $(function () {
       data: vacaInfo
     })
     .then(() => {
-      location.reload();
+      //CHECK IF THIS (LOCATION.RELOAD();) IS WHAT IS FORCING A RELOAD OF THE PAGE BECAUSE I DONT WANT THAT TO HAPPEN WHILE I AM WORKING WITH THE DATA FOR NOW - JORDAN
+
+      // location.reload();
+      console.log("reload prevented! Nice find, Jordan!");
     })
     .catch(err => console.log(err));
 
