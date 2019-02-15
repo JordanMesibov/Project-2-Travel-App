e678a0c5419aa3f1fec9214b9ef5a9fd60fa825e
Priyesh <91priyesh@gmail.com>
2019-02-15 13:23:14 -0500

    added navbar, added buttons to dashboard, added logoutbutton to navbar, added html/js code to identify pressence of logged into the website. temp cssard to test linking while in class

--- public/js/creategroup.js
+++ public/js/creategroup.js
@@ -1,3 +1,20 @@
+//NavBar Code Start
+$.ajax({
+  url: '/api/users/status',
+  method: 'GET'
+}).then(function(userInfo) {
+  console.log(userInfo);
+  $("#user-name").text(userInfo.full_name);
+  $("#user-info")
+    
+    .append(`<p>Welcome: ${userInfo.userName}</p>`)
+    .append(`<p>Email: ${userInfo.email}</p>`)
+})
+.catch(err => console.log(err));
+//NavBar Code End
+
+
+
 $(function () {
   $("#submit").on("click", function (event) {
     event.preventDefault();
