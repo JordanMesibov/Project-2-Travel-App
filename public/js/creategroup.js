fa21981ac8fdcdd762faabcbf79becae173ae3a8
CharityQuinn <charityaquinn@gmail.com>
2019-02-17 17:01:32 -0500

    Added the retrieval of the groupName as data saved

--- public/js/creategroup.js
+++ public/js/creategroup.js
@@ -19,15 +19,16 @@ $(function () {
   $("#submit").on("click", function (event) {
     event.preventDefault();
 
-    let groupName = $("#groupName").val().trim();
+    const groupName = $("#groupName").val().trim();
     console.log("The groupName is:");
     console.log(groupName);
-
+    
     let city1Name = $("#city1").val().trim();
     let city2Name = $("#city2").val().trim();
     let city3Name = $("#city3").val().trim();
     let city4Name = $("#city4").val().trim();
     let city5Name = $("#city5").val().trim();
+    
     console.log("The cities that have been entered are:");
     console.log(city1Name);
     console.log(city2Name);
@@ -35,13 +36,13 @@ $(function () {
     console.log(city4Name);
     console.log(city5Name);
 
-    let vacaInfo = {
+    const vacaInfo = {
       city1: city1Name,
       city2: city2Name,
       city3: city3Name,
       city4: city4Name,
       city5: city5Name,
-      groupId: this.groupName
+      groupId: groupName
     };
 
     console.log("This is creategroups groupId "+ groupId);
@@ -67,18 +68,32 @@ $(function () {
       event.preventDefault();
       //capture the input in the addGroupMembers form field
       let newGroupMember = $("#addGroupMembers").val().trim();
-      //--------------------------------------------------------------------
-      //right here is where we need to POST this user to the DB, before we reassign the newGroupMember variable!
-        // ACTUALLY: SET IT UP SO THAT THE NEW GROUP MEMBERS ARE PUSHED TO AN EMPTY ARRAY, THEN HAVE THE PUT ROUTE OF THESE NEW USERS BE PUT INTO A GROUP ON CLICK OF THE SUBMIT BUTTON, NOT ON CLICK OF THE ADD MEMBERS BUTTON!
-
-      //--------------------------------------------------------------------
       console.log("New Group Member below!");
       console.log(newGroupMember);
+
       //grab the div with an ID of addedMembers and prepend the newGroupMember to it
       $(".addedMembers").prepend(newGroupMember);
       $(".addedMembers").prepend("<br>");
       $("#addGroupMembers").val("");
 
+      //--------------------------------------------------------------------
+
+      //right here is where we need to POST this user to the DB, before we reassign the newGroupMember variable!
+        // ACTUALLY: SET IT UP SO THAT THE NEW GROUP MEMBERS ARE PUSHED TO AN EMPTY ARRAY, THEN HAVE THE PUT ROUTE OF THESE NEW USERS BE PUT INTO A GROUP ON CLICK OF THE SUBMIT BUTTON, NOT ON CLICK OF THE ADD MEMBERS BUTTON!
+
+      //--------------------------------------------------------------------
+      $.ajax({
+        url: '/api/groups',
+        method: 'POST',
+        data: groupName,
+      })
+      .then(() => {
+       
+        console.log("Loaded GroupName into groups" + data);
+      })
+      .catch(err => console.log(err));
     });
-  
+
 });
+  
+
