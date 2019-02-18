a3fa201f84ba7d777f376ca2e2f47872123c51da
unknown <pp_nancy@hotmail.com>
2019-02-18 15:27:42 -0500

    reform API group.js and usergroupController.js

--- controllers/usergroupController.js
+++ controllers/usergroupController.js
@@ -1,6 +1,7 @@
 const db = require('../models');
 
 module.exports = {
+
   findAll: function(req, res) {
     db
       .UserGroup
@@ -24,5 +25,79 @@ module.exports = {
         console.log(err);
         res.status(404).json(err);
       });
-  }
+  },
+  delete:function(req, res) {
+    db
+    .Group
+    .destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(result) {
+        res.json(result);
+      });
+  },
+  create: function(req, res) {
+    db
+    .Group
+    .create({
+      // Group rows here
+      // vacations: req.body.vacations,
+      name: req.body.name
+    })
+      .then(function(result) {
+        res.json(result);
+      });
+ 
+  
 }
+// nan add
+
+
+
+//Nan can not do the below this because 2 findAlls 
+
+// Get route for returning specific group info
+// router.get("/:id", function(req, res) {
+//   db.Group.findAll({
+//     include: [
+//       {
+//         model: db.User,
+//         through: db.Usergroup
+//         },
+//       db.VacationOptions
+//     ],
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then(function(result) {
+//       res.json(result);
+//     });
+// });
+// // Get route for returning specific group info
+// router.get("/name/:name", function(req, res) {
+//   db.Group.findAll({
+//     include: [
+//       {
+//         model: db.User,
+//         through: db.Usergroup
+//         },
+//       db.VacationOptions
+//     ],
+//     where: {
+//       name: req.params.name
+//     }
+//   })
+//     .then(function(result) {
+//       res.json(result);
+//     });
+// });
+
+// // POST route for creating new group
+// // router.post("/", function(req, res) {
+// //   console.log(req.body);
+  
+
+
--- routes/api/groups.js
+++ routes/api/groups.js
@@ -2,77 +2,94 @@ var db = require("../../models");
 var express = require('express');
 var router  = express.Router();
 
+//nan add here
+const usergroupController = require('../../controllers/usergroupController.js');
 
+router
+  .route('/')
+  .get(usergroupController.findAll)
+  .post(usergroupController.create);
+  
 
+router
+  .route('/:id')
+  .get(usergroupController.findAll)
+  .delete(usergroupController.destroy);
+
+router
+  .route('/name/:name')
+  .get(usergroupController.findAll);
+
+// -----------------------------------------------
   // GET route for getting all of the groups
-  router.get("/", function(req, res) {
-    db.Group.findAll({})
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+  // router.get("/", function(req, res) {
+  //   db.Group.findAll({})
+  //     .then(function(result) {
+  //       res.json(result);
+  //     });
+  // });
 
   // Get route for returning specific group info
-  router.get("/:id", function(req, res) {
-    db.Group.findAll({
-      include: [
-        {
-          model: db.User,
-          through: db.Usergroup
-          },
-        db.VacationOptions
-      ],
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+//   router.get("/:id", function(req, res) {
+//     db.Group.findAll({
+//       include: [
+//         {
+//           model: db.User,
+//           through: db.Usergroup
+//           },
+//         db.VacationOptions
+//       ],
+//       where: {
+//         id: req.params.id
+//       }
+//     })
+//       .then(function(result) {
+//         res.json(result);
+//       });
+//   });
 
-// Get route for returning specific group info
-  router.get("/name/:name", function(req, res) {
-    db.Group.findAll({
-      include: [
-        {
-          model: db.User,
-          through: db.Usergroup
-          },
-        db.VacationOptions
-      ],
-      where: {
-        name: req.params.name
-      }
-    })
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+// // Get route for returning specific group info
+//   router.get("/name/:name", function(req, res) {
+//     db.Group.findAll({
+//       include: [
+//         {
+//           model: db.User,
+//           through: db.Usergroup
+//           },
+//         db.VacationOptions
+//       ],
+//       where: {
+//         name: req.params.name
+//       }
+//     })
+//       .then(function(result) {
+//         res.json(result);
+//       });
+//   });
 
-  // POST route for creating new group
-  router.post("/", function(req, res) {
-    console.log(req.body);
-    db.Group.create({
-      // Group rows here
-      // vacations: req.body.vacations,
-      name: req.body.name
-    })
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+//   // POST route for creating new group
+//   router.post("/", function(req, res) {
+//     console.log(req.body);
+//     db.Group.create({
+//       // Group rows here
+//       // vacations: req.body.vacations,
+//       name: req.body.name
+//     })
+//       .then(function(result) {
+//         res.json(result);
+//       });
+//   });
 
-  // DELETE route for deleting group
-  router.delete("/:id", function(req, res) {
-    db.Group.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+//   // DELETE route for deleting group
+//   router.delete("/:id", function(req, res) {
+//     db.Group.destroy({
+//       where: {
+//         id: req.params.id
+//       }
+//     })
+//       .then(function(result) {
+//         res.json(result);
+//       });
+//   });
 
 module.exports = router;
