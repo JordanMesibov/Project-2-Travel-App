6e34604b039687b4f37182752f23683ab3ee5874
Priyesh <91priyesh@gmail.com>
2019-02-18 14:52:04 -0500

    dynamic population of groups on dashboard working

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,37 +1,57 @@
 console.log("dashboard.js linked!");
-
 let groupLink = {
   GroupId: 0,
   UserId: 0
 }
-
 let isLeader = {
   isLeader: true
 }
-console.log(groupLink);
+
+let temp;
+// console.log(groupLink);
 $(document).ready(function () {
-
-
   //NavBar Code Start
   $.ajax({
-      url: '/api/users/status',
-      method: 'GET'
-    }).then(function (userInfo) {
-      console.log(userInfo);
-      $("#user-name").text(userInfo.full_name);
-      $("#user-info")
+    url: '/api/users/status',
+    method: 'GET'
+  }).then(function (userInfo) {
+    // console.log(userInfo);
+    $("#user-name").text(userInfo.full_name);
+    $("#user-info")
 
-        .append(`<p>Welcome: ${userInfo.userName}</p>`)
-        .append(`<p>Email: ${userInfo.email}</p>`)
+      .append(`<p>Welcome: ${userInfo.userName}</p>`)
+      .append(`<p>Email: ${userInfo.email}</p>`)
 
-      groupLink.UserId = userInfo.id;
-      console.log(groupLink);
-    })
-    .catch(err => console.log(err));
+    groupLink.UserId = userInfo.id;
+    // console.log(groupLink);
+
+    //ajaxcall for group info on the user that is logged in
+    console.log(groupLink.UserId);
+    $.ajax({
+        url: `/api/users/id/${groupLink.UserId}}`,
+        method: 'GET'
+      }).then(function (groupInfo) {
+        // console.log(groupInfo);
+        // console.log(groupInfo.Groups);
+        let rawGroupsArray = groupInfo.Groups;
+        console.log("rawgroup");
+        console.log(rawGroupsArray);
+        const newGroupArray = rawGroupsArray.map(element => element.name);
+        console.log("newGroupArray");
+        console.log(newGroupArray);
+
+        // for (i = 0; i < newGroupArray.length; i++) {
+        //   $("<div class='groups' />").text(newGroupArray[i]).appendTo(".groupdivs");
+        // }
+        for (i = 0; i < newGroupArray.length; i++) {
+          $("<div class='groups' />").text(newGroupArray[i]).attr("data-id", newGroupArray[i]).appendTo(".groupdivs");
+        }
+
+
+      })
+      .catch(err => console.log(err));
+  });
   //NavBar Code End
-
-
-
   $(".submitcreategroup").on("click", function (event) {
     event.preventDefault();
     // alert( "CG clicked" );
@@ -47,13 +67,10 @@ $(document).ready(function () {
       // console.log(groupId);
       // console.log(groupId.id);
       groupLink.GroupId = groupId.id;
-      console.log(groupLink);
+      // console.log(groupLink);
       updateLeader();
       postIds();
       location.reload();
-
-
-
     });
 
     function postIds() {
@@ -63,7 +80,7 @@ $(document).ready(function () {
         method: "POST",
         data: groupLink // req.body
       }).then(function (response) {
-        console.log(response);
+        // console.log(response);
       });
     };
 
@@ -73,15 +90,12 @@ $(document).ready(function () {
         method: "PUT",
         data: isLeader
       }).then(function (response) {
-        console.log(response);
+        // console.log(response);
       });
     };
-    
-
   });
 
   $(".joinGroup").click(function () {
     alert("CG clicked");
   });
-
 });
